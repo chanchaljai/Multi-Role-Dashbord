878452a37de10e30cd1fe688995bd6c878711016
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-20 15:02:07 +0530

    user

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
+import mongoose, { connect } from "mongoose";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
